d6c5f22c57879c9e208cf150bfaf6cd96619253b
halilili00 <halilkund68@gmail.com>
2022-08-26 17:15:32 +0300

    Connected to mongoDb

--- server/index.js
+++ server/index.js
@@ -2,3 +2,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL)
+.then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+.catch((error) => console.group(error.message))
